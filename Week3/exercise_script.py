461cf9f54182d930a8b494021ca4018fd0aea452
Finn <finley.currie224@gmail.com>
2025-03-26 21:58:11 +0000

    exercise complete
    - load data & apply projection
    - spatial join
    - create map

--- Week3/exercise_script.py
+++ Week3/exercise_script.py
@@ -14,12 +14,30 @@ import matplotlib.patches as mpatches
 
 # your analysis goes here...
 
+
+counties = gpd.read_file('d:\GIS_Course\EGM722\Week3\data_files\Counties.shp').to_crs(32629)
+wards = gpd.read_file('d:\GIS_Course\EGM722\Week3\data_files\\NI_Wards.shp').to_crs(32629)
+
+join = gpd.sjoin(counties, wards, how='inner', lsuffix='left', rsuffix='right') # perform the spatial join
+
+print(join.groupby(['CountyName'])['Population'].sum()) # highest popn: Antrim (718369), lowest popn: Fermanagh (75906)
+
+
 # ---------------------------------------------------------------------------------------------------------------------
 # below here, you may need to modify the script somewhat to create your map.
 # create a crs using ccrs.UTM() that corresponds to our CRS
+
+def generate_handles(labels, colors, edge='k', alpha=1):
+    lc = len(colors)  # get the length of the color list
+    handles = [] # create an empty list
+    for ii in range(len(labels)): # for each label and color pair that we're given, make an empty box to pass to our legend
+        handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[ii % lc], edgecolor=edge, alpha=alpha))
+    return handles
+
+
 ni_utm = ccrs.UTM(29)
 
-# create a figure of size 10x10 (representing the page size in inches
+# # create a figure of size 10x10 (representing the page size in inches
 fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=ni_utm))
 
 # add gridlines below
@@ -47,4 +65,4 @@ county_handles = generate_handles([''], ['none'], edge='r')
 ax.legend(county_handles, ['County Boundaries'], fontsize=12, loc='upper left', framealpha=1)
 
 # save the figure
-fig.savefig('sample_map.png', dpi=300, bbox_inches='tight')
+# fig.savefig('sample_map.png', dpi=300, bbox_inches='tight')
